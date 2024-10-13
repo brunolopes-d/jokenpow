33ceec304446bee0373a125ea54dc26175e0b483
Bruno Lopes <bruno.lopes@dcomp.ufs.br>
2024-10-13 11:33:33 -0300

    HP + Nome e Alteração Personagens
    Adicionado o nome acima da barra de HP, além de aumentar ela. Além disso, houve a troca dos sprites dos personagens.

--- js/jogo.js
+++ js/jogo.js
@@ -4,10 +4,20 @@ const params = new URLSearchParams(window.location.search);
 const htmlPersonagemP1 = document.querySelector('.personagemP1')
 const htmlPersonagemP2 = document.querySelector('.personagemP2')
 
+const p1 = params.get("personagem1")
+const p2 = params.get("personagem2")
+
+const nomeP1 = document.querySelector('.personagemNomeP1')
+const nomeP2 = document.querySelector('.personagemNomeP2')
+
+nomeP1.innerText = p1
+nomeP2.innerText = p2
+
+
 // Mudar o src dos personagens para as imagens corresponderem aos personagens escolhidos, com
 // os query params que pegamos da url.
-htmlPersonagemP1.setAttribute('src', `assets/images/personagens/${params.get('personagem1')}.png`)
-htmlPersonagemP2.setAttribute('src', `assets/images/personagens/${params.get('personagem2')}.png`)
+htmlPersonagemP1.setAttribute('src', `assets/images/personagens/${p1}.png`)
+htmlPersonagemP2.setAttribute('src', `assets/images/personagens/${p2}.png`)
 
 const barraVidaJogador1 = document.querySelector('.player1-health');
 const barraVidaJogador2 = document.querySelector('.player2-health');
