ada2014afa36e0fda0b8e0a7319772ea8ae6673b
Jeferson1917 <jeferson.filho@dcomp.ufs.br>
2024-10-19 21:51:18 -0300

    atualização

--- js/vitoriaDerrota.js
+++ js/vitoriaDerrota.js
@@ -4,24 +4,16 @@ const ganhador = params.get("ganhador");
 const perdedor = params.get("perdedor");
 
 const vitoriaDiv = document.querySelector(".vencedor")
-const piadinha = document.querySelector(".piadinha")
 vitoriaDiv.innerHTML = `A ${ganhador} saiu vitoriosa.`
-const vitoriaDiv = document.querySelector(".vencedor");
 const piadinha = document.querySelector(".piadinha");
 
 vitoriaDiv.innerHTML = `A ${ganhador} saiu vitoriosa.`;
 
-if (ganhador === "Papel" && perdedor === "Tesoura") {
-    piadinha.innerHTML = "Pelo visto o papel tava mais afiado do que a lâmina da tesoura."
-}
 // Captura o botão 'Voltar para a tela inicial'
 document.querySelector('.voltarTelaInicial').addEventListener('click', function() {
     window.location.href = 'index.html'; // Redireciona para index.html ao clicar no botão
 });
 
-if (ganhador === "Tesoura" && perdedor === "Pedra") {
-    piadinha.innerHTML = "Essa tesoura é mais afiada do que o normal, a pedra não teve chance alguma."
-// Ação do botão 'Revanche'
 document.querySelector('.revanche').addEventListener('click', function() {
     // Redireciona para arena.html com os personagens e o ganhador
     window.location.href = `arena.html?ganhador=${ganhador}&perdedor=${perdedor}`;
